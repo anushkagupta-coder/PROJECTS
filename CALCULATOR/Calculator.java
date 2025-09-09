a385ff8081e4f1fd0ca3dec80059d27c8ff4bdd4
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-10 01:14:11 +0530

    Update Calculator.java

--- CALCULATOR/Calculator.java
+++ CALCULATOR/Calculator.java
@@ -14,7 +14,7 @@ public class Calculator implements ActionListener {
 
     Font myFont = new Font("Ink Free",Font.BOLD,30);
 
-    double num1=0;num2=0,result=0;
+    double num1=0,num2=0,result=0;
     char operator;
 
     Calculator(){
