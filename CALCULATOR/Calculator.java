87f9960f93a83fab0f41e1571fa8affaf96ab49a
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-10 00:32:07 +0530

    Update Calculator.java

--- CALCULATOR/Calculator.java
+++ CALCULATOR/Calculator.java
@@ -1,4 +1,8 @@
 //I AM MAKING A CALCULATOR USING JAVA GUI;
-public class Calculator {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener {
     
 }
