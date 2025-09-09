73d2130aa4fa8c27fcd6c8cb8184ca3044f64617
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-10 00:44:42 +0530

    Update Calculator.java

--- CALCULATOR/Calculator.java
+++ CALCULATOR/Calculator.java
@@ -4,5 +4,40 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Calculator implements ActionListener {
-    
+    JFrame frame;
+    Jtextfeild textfeild;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[10];
+    JButton addButton,subButton,mulButton,divButton;
+    JButton decButton,equButton,delButton,clrButton,negButton;
+    JPanel panel;
+
+    Font myFont = new Font("Ink Free",Font.BOLD,30);
+
+    double num1=0;num2=0,result=0;
+    char operator;
+
+    Calculator(){
+        frame = new JFrame("CALCULATOR");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setsize(420,550);
+        frame.setLayout(null);
+
+        textfield=new JTextField();
+        textfield.setBounds(50,25,300,50);
+        textfield.setFont(myFont);
+        textfield.setEditable(false);
+
+        addButton=new JButton("+");
+        subButton=new JButton("-");
+        mulButton=new JButton("*");
+        divButton=new JButton("/");
+        decButton=new JButton(".");
+        equButton=new JButton("=");
+        delButton=new JButton("DELETE");
+        clrButton=new JButton("CLEAR");
+        addButton=new JButton("+");
+
+
+    }
 }
