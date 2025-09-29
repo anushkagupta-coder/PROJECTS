b4bc64f867e1d3c888522dda0c66c1e459ce7a6a
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-30 00:04:44 +0530

    .

--- TemperatureConversion/index.js
+++ TemperatureConversion/index.js
@@ -14,7 +14,9 @@ function convert(){
         result.textContent=temp.toFixed(1)+"F"
     }
     else if(toCelsius.checked){
-        result.textContent="You selected to Celcius";
+        temp=Number(textBox.value);
+        temp= (temp - 32)*(5 / 9)
+        result.textContent=temp.toFixed(1)+"C"
     }
     else{
         result.textContent="select a unit";
