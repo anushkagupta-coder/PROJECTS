0cd93cf38ccfdfedc9e5fb6845984eb42b05dd3c
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-01 17:16:25 +0530

    Update index.js

---
+++ RollingDice/index.js
@@ -0,0 +1,13 @@
+
+function rollDice() {
+    const numOfDice=document.getElementById("numOfValue").value;
+    const diceResult=document.getElementById("diceResult");
+    const diceImage=document.getElementById("diceImage");
+    const values=[];
+    const images=[];
+
+    for(let i=0;i<numOfDice;i++){
+        const value=Math.floor(Math.random()*6)+1;
+        
+    }
+}
