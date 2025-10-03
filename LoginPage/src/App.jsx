862b663bcd66c8f205fa2af09d7b6dd96cf1ae8f
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-03 20:12:46 +0530

    Update App.jsx

--- LoginPage/src/App.jsx
+++ LoginPage/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import signup from './signup'
+import login from './login.jsx'
+import signup from './signup.jsx'
 import {BrowserRouter , Routes, Route} from 'react-router-dom'
 function App() {
 
