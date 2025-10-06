4edb5ab0725c374ce24a6669fd59eb2efa0d47fa
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-06 20:30:58 +0530

    authentication

---
+++ Authentication/backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { JsonWebTokenError } from "jsonwebtoken";
+import mongoose from "mongoose";
+import dotenv, { configDotenv } from "dotenv";
+import cors from "cors";
+import AuthRoutes from "./routes/Auth.js";
+
+dotenv.config();
+const app= express();
+app.use(cors());
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true})
+    .then(() => console.log("MongoDB connected"))
+    .then(err => console.log(err))
+
+    // Use auth routes
+app.use("/api/auth", AuthRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
+
+
